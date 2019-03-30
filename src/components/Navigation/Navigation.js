efe4abc7cf0fd76321264e4d92771fd1cbf545ff
jackris06 <jackris06@gmail.com>
2019-03-30 06:45:26 -0600

    Adding Components

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+const Navigation = ({ onRouteChange,isSignedIn })=> {
+	if(isSignedIn){
+		return(
+			<nav style={{display: 'flex', justifyContent:'flex-end'}}>
+				<p
+				onClick={() => onRouteChange('signout')} 
+				className='f3 link dim black no-underline pa3 pointer'> Sign Out </p>
+			</nav>
+		);
+	} else {
+		return(
+			<div>
+				<nav style={{display: 'flex', justifyContent:'flex-end'}}>
+					<p
+					onClick={() => onRouteChange('signin')} 
+					className='f3 link dim black no-underline pa3 pointer'> Sign in </p>
+					<p
+					onClick={() => onRouteChange('register')} 
+					className='f3 link dim black no-underline pa3 pointer'> Register </p>					
+				</nav>
+			</div>	
+		);				 
+	}
+}
+
+
+export default Navigation;
